53b0c21b2b21c55629b53db6c5e623ff883fbc9f
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-08-27 18:17:13 -0500

    Se le realizaron cambios al main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ function simulador() {
     contPre.classList.add('disabled');
 
     //Variables que capturan los valores de los campos
-    let clNombres = document.getElementById('nombres').values;
-    let clTelefono = document.getElementById('telefono').values;
-    let clEmail = document.getElementById('email').values;
-    let clMonto = document.getElementById('monto').values;
-    let clTiempo = document.getElementById('tiempo').values;
+    let clNombres = document.getElementById('clNombres').value;
+    let clTelefono = document.getElementById('clTelefono').value;
+    let clEmail = document.getElementById('clEmail').value;
+    let clMonto = document.getElementById('clMonto').value;
+    let clTiempo = document.getElementById('clTiempo').value;
 
     //Constantes que capturan las etiquetas span, en las que se muestran los valores (show)
     const showTiempo = document.getElementById('showTiempo');
@@ -19,9 +19,49 @@ function simulador() {
     const showGanancia = document.getElementById('showGanancia');
     const showNombres = document.getElementById('showNombres');
     const showCorreo = document.getElementById('showCorreo')
+
+    //Variables para realizar los calculos
+    let ganancia = 0
+    let total = 0
+
+    switch (clTiempo) {
+        case '1':
+            //en este caso realizaremos el calculo si el tiempo corresponde a un año
+        ganancia = (clMonto*0.008)*12;
+        total = parseInt(clMonto) + parseInt(ganancia);
+        showInteres.textContent = '0.8%';
+        showTiempo.textContent = '12 MESES';
+
+            break;
+
+        case '2':
+            //en este caso realizaremos el calculo si el tiempo corresponde a dos años
+        ganancia = (clMonto*0.013)*24;
+        total = parseInt(clMonto) + parseInt(ganancia);
+        showInteres.textContent = '1.3%';
+        showTiempo.textContent = '24 MESES';
+            break;
+
+        case '3':
+            //en este caso realizaremos el calculo si el tiempo corresponde a tres años
+        ganancia = (clMonto*0.017)*36;
+        total = parseInt(clMonto) + parseInt(ganancia);
+        showInteres.textContent = '1.7%';
+        showTiempo.textContent = '36 MESES';
+            break;
+        
+    }
+
+  
+
+    showTotal.textContent = total;
+    showGanancia.textContent = ganancia;
+    showNombres.textContent = clNombres;
+    showCorreo.textContent = clEmail;
+
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('calcular').addEventListener('click', simulador);
 });
 
